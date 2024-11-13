7269f35de8daf6db4b376d9be6c50e1cfcdd4152
Ismael Seck <ismaelseck11@gmail.com>
2024-11-13 01:53:05 +0000

    final backend commit

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,13 +4,12 @@ import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './shemas/user.schema';
 import { RefreshToken } from './shemas/refresh-token.schema';
-import { MailService } from 'src/services/mail.services';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User, RefreshToken])
   ],
   controllers: [AuthController],
-  providers: [AuthService, MailService],
+  providers: [AuthService],
 })
 export class AuthModule {}
